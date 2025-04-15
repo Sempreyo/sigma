1f501e6b4ca143f379c27f5295bd69fc71dd4583
ArtZ <snickgem@gmail.com>
2025-04-15 18:31:47 +0300

    fix

--- src/js/partials/swiper-init.js
+++ src/js/partials/swiper-init.js
@@ -17,4 +17,27 @@ document.addEventListener("DOMContentLoaded", () => {
 			});
 		});
 	}
+
+	const articlesCarousel = document.querySelectorAll(".other-articles__slider");
+
+	if (articlesCarousel.length > 0) {
+		articlesCarousel.forEach(el => {
+			const articlesCarouselNext = el.nextElementSibling.querySelector(".swiper-btn-next");
+			const articlesCarouselPrev = el.nextElementSibling.querySelector(".swiper-btn-prev");
+
+			const slider = new Swiper(el, {
+				slidesPerView: 1,
+				spaceBetween: 10,
+				navigation: {
+					nextEl: articlesCarouselNext,
+					prevEl: articlesCarouselPrev,
+				},
+				breakpoints: {
+					576: {
+						slidesPerView: 2,
+					}
+				}
+			});
+		});
+	}
 });
